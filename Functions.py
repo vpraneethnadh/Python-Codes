784520b868fd4fcab0225f5d319586a57d42698c
Vudattu Praneethnadh <142204604+vpraneethnadh@users.noreply.github.com>
2024-10-18 13:47:34 +0530

    Add files via upload

---
+++ Functions.py
@@ -0,0 +1,35 @@
+def greet():
+    print("HELLO")
+    print("GOOD MORNING")
+
+greet()
+
+def sum(x,y):
+    c=x+y
+    print(c)
+
+sum(4,5)
+
+def diff(x,y):
+    c=x-y
+    print(c)
+
+diff(5,4)
+
+def mul(x,y):
+    c=x*y
+    print(c)
+
+mul(4,5)
+
+def idiv(x,y):
+    c=x//y
+    print(c)
+
+idiv(5,4)
+
+def div(x,y):
+    c=x/y
+    print(c)
+
+div(5,4)
