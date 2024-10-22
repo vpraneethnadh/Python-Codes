62984793b61eb0c0e0d60c95a2ff95e90af89157
Vudattu Praneethnadh <142204604+vpraneethnadh@users.noreply.github.com>
2024-10-22 09:06:18 +0530

    Add files via upload

---
+++ Greet.py
@@ -0,0 +1,37 @@
+def greet():
+    print("HELLO")
+    print("GOOD MORNING")
+    print("\n")
+
+
+def add(x, y):
+    c = x + y
+    print(c)
+
+
+def sub(x, y):
+    c = x - y
+    print(c)
+
+
+def mul(x, y):
+    c = x * y
+    print(c)
+
+
+def idiv(x, y):
+    c = x // y
+    print(c)
+
+
+def div(x, y):
+    c = x / y
+    print(c)
+
+
+greet()
+add(4, 5)
+sub(5, 4)
+mul(4, 5)
+idiv(5, 4)
+div(5, 4)
