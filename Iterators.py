87dd811c8469414a19ba874d3f5160f319a22a59
Vudattu Praneethnadh <142204604+vpraneethnadh@users.noreply.github.com>
2024-10-28 11:40:25 +0530

    Add files via upload

---
+++ Iterators.py
@@ -0,0 +1,12 @@
+name = ["praneeth", "adithya", "kanthi", "kiran"]
+
+# USING ITETATORS
+it = iter(name)
+
+print(next(it))
+print(next(it))
+print(next(it))
+
+# USING LOOP
+for x in name:
+    print(x)
