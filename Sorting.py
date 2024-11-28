c1615a6aa9a7f46f67b332ad665aa15536026602
Vudattu Praneethnadh <142204604+vpraneethnadh@users.noreply.github.com>
2024-11-28 14:43:59 +0530

    Add files via upload

---
+++ Sorting.py
@@ -0,0 +1,33 @@
+print("IN ASSENDING ORDER:-")
+numbers=[1,3,4,5,6,2]
+numbers.sort()
+print(numbers)
+
+decimalnumbers=[1.234,2.345,4.812,4.813,0.123]
+decimalnumbers.sort()
+print(decimalnumbers)
+
+alphabet=['A','J','I','H','G','F','D','E','C','B']
+alphabet.sort()
+print(alphabet)
+
+words=['Praneeth','praneeth','Python','Laptop']
+words.sort()
+print(words)
+
+print("IN DESENDING ORDER:-")
+numbers=[1,3,4,5,6,2]
+numbers.sort(reverse=True)
+print(numbers)
+
+decimalnumbers=[1.234,2.345,4.812,4.813,0.123]
+decimalnumbers.sort(reverse=True)
+print(decimalnumbers)
+
+words=['Praneeth','praneeth','Python','Laptop']
+words.sort(reverse=True)
+print(words)
+
+alphabet=['A','J','I','H','G','F','D','E','C','B']
+alphabet.sort(reverse=True)
+print(alphabet)
